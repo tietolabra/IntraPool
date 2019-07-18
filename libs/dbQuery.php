6a65d24249436ba4eb76fd56b68e8e33326eb207
Tietolabra <tietolabra@hamk.fi>
2019-07-18 06:05:29 +0000

    Added the needed global

--- libs/dbQuery.php
+++ libs/dbQuery.php
@@ -4,6 +4,7 @@ require_once 'db.php';
 require_once 'json.php';
 
 function queryCompanies($query) {
+	global $db;
 	$stmt = $db->prepare("SELECT * FROM `companies` WHERE `name` = ?");
 	$stmt->bind_param("s", $query);
 	$stmt->execute();
