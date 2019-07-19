3dd05dbb0437d828a9a0f253c1ec61dd26ccac69
Petri Salo <petri.salo@hamk.fi>
2019-07-19 12:39:45 +0300

    Test code for adding new pools

---
+++ libs/addPool.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'db.php';
+require_once 'userData.php';
+
+function addPool() {
+    global $db;
+    global $userData;
+
+    foreach ($_POST as param) {
+        print(param);
+    }
+}
+
+?>
