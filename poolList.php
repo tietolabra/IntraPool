8ad0a751f5a634aed02958ec82682b22e8ed9438
Jukka Nieminen <jukka.nieminen@student.hamk.fi>
2019-07-19 15:37:41 +0300

    Fixed bugs

--- poolList.php
+++ poolList.php
@@ -59,7 +59,7 @@ function showMails(){
       emailElements[i].setAttribute("style", "display:inline;");
       emailElements[i].setAttribute("style", "display:inline;");
     }
-    var loactionElements = document.getElementById('roundTable').getElementsByClassName('location');
+    var locationElements = document.getElementById('roundTable').getElementsByClassName('location');
     for(var i = 0, length = locationElements.length; i < length; i++) {
       locationElements[i].setAttribute("style", "display:none;");
       locationElements[i].parentNode.style.display = "none";
