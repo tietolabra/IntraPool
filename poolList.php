91c2e59709cb29d2ad5a9a77a5ba44d09cae21d6
Jukka Nieminen <jukka.nieminen@student.hamk.fi>
2019-07-19 17:25:18 +0300

    Testing changes for mobile device in poolList

--- poolList.php
+++ poolList.php
@@ -55,4 +55,18 @@
       locationElements[i].parentNode.setAttribute("style", "display:none;");
     }
   }
+
+  function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
 </script>
