bdbf462cf8e546f74fd18ef812b1cca9a740b7f4
Jukka Nieminen <jukka.nieminen@student.hamk.fi>
2019-07-19 12:02:48 +0300

    Check for logoutButton CSS working

--- libs/UIElements.php
+++ libs/UIElements.php
@@ -2,7 +2,14 @@
 
 function getLogoutButton() {
     $js = "window.location.href='logout.php'";
-    echo '<button id="logoutButton" onclick="'.$js.'">Logout</button>';
+    echo '<button style="background-color: lightblue;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;" onclick="'.$js.'">Logout</button>';
 }
 
 ?>
