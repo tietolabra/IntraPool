61f974ed6257356e50829ee2349d9f8a4ac1820e
Jukka Nieminen <jukka.nieminen@student.hamk.fi>
2019-07-19 11:51:08 +0300

    Made form have start and enddate and weekday-selector for poolCreator.html

--- poolCreator.php
+++ poolCreator.php
@@ -8,8 +8,20 @@
 <h2 style="text-align: center">Carpool created using information submitted by you during registration
     and is now visible to people working at your company!</h2>
 <form>
-    <h2 style="text-align: center">Give the days when you are able to offer carpools.
+    <h2 style="text-align: center">Give the date-interval and weekdays when you are able to offer carpools.
     </h2>
-    <input type="date" name="dateData" id="dateData">
+    <input type="date" name="startDate" id="dateData">
+    -
+    <input type="date" name="endDate" id="dateData">
+    <br>
+    <select name="weekDays" size="7" multiple>
+    <option value="monday">Monday</option>
+    <option value="tuesday">Tuesday</option>
+    <option value="wednesday">Wednesday</option>
+    <option value="thursday">Thursday</option>
+    <option value="friday">Friday</option>
+    <option value="saturday">Saturday</option>
+    <option value="sunday">Sunday</option>
+</select>
     <input style="margin-left: 5%" type="button" value="Show rides" onclick="showRides()">
 </form>
