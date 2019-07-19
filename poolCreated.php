935d6703b29a786fd30f336ddd62cacbf49e8169
Jukka Nieminen <jukka.nieminen@student.hamk.fi>
2019-07-19 12:18:49 +0300

    Improved look of poolCreator.php for mobile

--- poolCreated.php
+++ poolCreated.php
@@ -10,4 +10,4 @@
 <form>
     <h2 style="text-align: center">Press the button to see carpools of your company</h2>
     <input style="margin-left: 5%" type="button" value="Show rides" onclick="showRides()">
-</form>
+</form
