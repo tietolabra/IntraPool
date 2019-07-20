9df7a90071468cd6a8fca3dc3e0c763da6e856b6
Petri Salo <petri.salo@petrimalja.com>
2019-07-20 09:41:46 +0300

    Only show top panel if user has logged in

--- index.php
+++ index.php
@@ -26,13 +26,14 @@ require 'libs/phpErrors.php'; ?>
 
 <body>
 
-<div class="container-fluid text-right fixed-top" style="background:rgba(255,255,255,0.2);"><div class="mt-md-2"><p class="align-middle">
+
                     <?php if (!empty($userData)) {
+                        echo '<div class="container-fluid text-right fixed-top" style="background:rgba(255,255,255,0.2);"><div class="mt-md-2"><p class="align-middle">';
                         echo 'Logged in as ' . $userData['name'];
                         getLogoutButton();
+                        echo '</p></div></div>';
                     } ?>
-                    </p>
-</div></div>
+                    
 
     <main class="container text-center">
         <div class="row justify-content-md-center">
